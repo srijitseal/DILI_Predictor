fcdc995e3db253b5f15ff958450ef79b12b03ef5
srijitseal <58182863+srijitseal@users.noreply.github.com>
2023-01-29 23:47:41 +0000

    Add files via upload

---
+++ Web_app.py
@@ -0,0 +1,307 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+from typing import Union
+from urllib.parse import unquote, quote
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import inchi
+from rdkit.Chem.MolStandardize import rdMolStandardize
+import pickle
+from mordred import Calculator, descriptors
+from rdkit.Chem.rdMolDescriptors import GetMorganFingerprintAsBitVect
+from sklearn.feature_selection import VarianceThreshold
+from itertools import compress
+from rdkit import Chem
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors as Descriptors
+import os
+from rdkit import Chem
+from rdkit import RDPaths
+from rdkit.Chem.Draw import IPythonConsole
+from rdkit.Chem import Draw
+from rdkit.Chem.Draw import rdMolDraw2D
+from rdkit.Chem.Draw import MolDraw2DSVG
+
+from bokeh.plotting import ColumnDataSource, figure, output_file, show
+from sklearn.decomposition import PCA
+from bokeh.io import output_notebook
+
+from molvs import standardize_smiles
+import pandas as pd
+from rdkit.Chem import inchi
+from rdkit import Chem
+
+import shap
+shap.initjs()
+
+def standardized_smiles(value):
+    #print(value)
+    try: return standardize_smiles(value)
+    except: return "Cannot_do"
+    
+def MorganFingerprint(s):
+    x = Chem.MolFromSmiles(s)
+    return (AllChem.GetMorganFingerprintAsBitVect(x,2,2048))
+
+
+def MACCSKeysFingerprint(s):
+    x = Chem.MolFromSmiles(s)
+    return (AllChem.GetMACCSKeysFingerprint(x))
+
+def get_num_charged_atoms_neg(mol):
+    mol_h = Chem.AddHs(mol)
+    Chem.rdPartialCharges.ComputeGasteigerCharges(mol_h)
+     
+    positive = 0
+    negative = 0
+     
+    for atom in mol_h.GetAtoms():
+        if float(atom.GetProp('_GasteigerCharge')) <= 0:
+            negative += 1
+     
+    return negative
+
+def get_num_charged_atoms_pos(mol):
+    mol_h = Chem.AddHs(mol)
+    Chem.rdPartialCharges.ComputeGasteigerCharges(mol_h)
+     
+    positive = 0
+    negative = 0
+     
+    for atom in mol_h.GetAtoms():
+        if float(atom.GetProp('_GasteigerCharge')) >= 0:
+            positive += 1
+    return positive
+
+def get_assembled_ring(mol):
+    ring_info = mol.GetRingInfo()
+    num_ring = ring_info.NumRings()
+    ring_atoms = ring_info.AtomRings()
+    num_assembled = 0
+     
+    for i in range(num_ring):
+        for j in range(i+1, num_ring):
+            x = set(ring_atoms[i])
+            y = set(ring_atoms[j])
+            if not x.intersection(y): # 2つの環が縮環でない場合に
+                for x_id in x:
+                    x_atom = mol.GetAtomWithIdx(x_id)
+                    neighbors = [k.GetIdx() for k in x_atom.GetNeighbors()]
+                    for x_n in neighbors:
+                        if x_n in y: # 環同士を繋ぐ結合があるか否か
+                            num_assembled += 1
+     
+    return num_assembled
+
+def get_num_stereocenters(mol):
+    return AllChem.CalcNumAtomStereoCenters(mol) + AllChem.CalcNumUnspecifiedAtomStereoCenters(mol)
+
+def calc_descriptors(dataframe):
+    mols = dataframe.smiles_r.apply(Chem.MolFromSmiles)
+    #mols_fps=[AllChem.GetMorganFingerprintAsBitVect(x,2) for x in mols]
+    descr = []
+    for m in mols:
+        descr.append([Descriptors.TPSA(m),
+               Descriptors.NumRotatableBonds(m),
+               AllChem.CalcNumRings(m),
+               Descriptors.NumAromaticRings(m),
+               Descriptors.NumHAcceptors(m),
+               Descriptors.NumHDonors(m),
+               Descriptors.FractionCSP3(m),
+               Descriptors.MolLogP(m) ,
+               Descriptors.NHOHCount(m),
+               Descriptors.NOCount(m),
+               Descriptors.NumHeteroatoms(m),
+               get_num_charged_atoms_pos(m),
+               get_num_charged_atoms_neg(m),
+               get_assembled_ring(m),
+               get_num_stereocenters(m)])
+    descr = np.asarray(descr)
+    return(descr)
+
+descs = [ 'PSA', 'n_rot_bonds', 'n_rings', 'n_ar_rings',
+         'n_HBA', 'n_HBD', 'Fsp3', 'logP', 'NHOHCount', 'NOCount', 'NumHeteroatoms',
+        'n_positive', '_n_negative', 'n_ring_asmbl', 'n_stereo']
+
+def calc_all_fp_desc(data):
+   
+    calc = Calculator(descriptors, ignore_3D=True)
+    print(len(calc.descriptors))
+    Ser_Mol = data['smiles_r'].apply(Chem.MolFromSmiles)
+    # as pandas
+    Mordred_table=  calc.pandas(Ser_Mol)
+    Mordred_table = Mordred_table.astype('float')
+    #Mordred_table['smiles_r'] = model_tox_data['smiles_r']
+    
+    MACCSfingerprint_array = np.stack(data['smiles_r'].apply(MACCSKeysFingerprint))
+    MACCS_collection = []
+    for x in np.arange(MACCSfingerprint_array.shape[1]):
+        x = "MACCS"+str(x)
+        MACCS_collection.append(x)
+    MACCSfingerprint_table = pd.DataFrame(MACCSfingerprint_array, columns=MACCS_collection)
+
+
+    MorganFingerprint_array = np.stack(data['smiles_r'].apply(MorganFingerprint))
+    Morgan_fingerprint_collection = []
+    for x in np.arange(MorganFingerprint_array.shape[1]):
+        x = "Mfp"+str(x)
+        Morgan_fingerprint_collection.append(x)
+    Morgan_fingerprint_table = pd.DataFrame(MorganFingerprint_array, columns=Morgan_fingerprint_collection)
+
+    
+    a=calc_descriptors(data)
+    descdf=pd.DataFrame(a, columns=descs)
+    descdf_approved=descdf.reset_index(drop=True)
+    descdf_approved
+    
+    tox_model_data= pd.concat([data, Morgan_fingerprint_table, MACCSfingerprint_table, descdf_approved, Mordred_table], axis=1)
+    tox_model_data
+    
+    return(tox_model_data)
+    
+    
+liv_data = [ "2",  "3",  "4",  "5",  "6",  "7",  "8",  "9", "10", "11", "14", "15", "16"]
+
+def predict_individual_liv_data(data_dummy, features, endpoint):#predict animal data
+    
+    
+
+    loaded_rf = pickle.load(open(f"bestlivmodel_{endpoint}_model.sav", 'rb'))
+
+    X = data_dummy[features]
+    X = X.values
+    
+    y_proba =  loaded_rf.predict_proba(X)[:,1]  
+    
+    return(y_proba)
+
+def predict_liv_all(data):
+    #Read columns needed for rat data
+    file = open(f"./features_morgan_mordred_maccs_physc.txt", "r")
+    file_lines = file.read()
+    features = file_lines.split("\n")
+    features = features[:-1]
+    
+    data_dummy = data
+
+    for endpoint in liv_data:
+        #print(endpoint)
+        y_proba = predict_individual_liv_data(data_dummy, features, endpoint) 
+        data[endpoint] = y_proba
+    
+    return(data)
+
+
+def predict_DILI(data):#log human_VDss_L_kg model
+    
+    #Read columns needed for rat data
+    file = open(f"./features_morgan_mordred_maccs_physc.txt", "r")
+    file_lines = file.read()
+    features = file_lines.split("\n")
+    features = features[:-1]
+    
+    features = list(features) + list(liv_data)
+    loaded_rf = pickle.load(open("./final_dili_model.sav", 'rb'))
+
+    X = data[features]
+    y_proba =  loaded_rf.predict_proba(X)[:,1]
+    best_thresh = 0.622537
+    print('Best Threshold=%f' % (best_thresh))
+    
+    y_pred  = [ 1 if y_proba>best_thresh  else 0] 
+    
+    explainer = shap.TreeExplainer(loaded_rf)
+    shap_values = explainer.shap_values(X)
+    
+
+    shap.force_plot(explainer.expected_value[1], 
+    shap_values[1], X.iloc[0],
+    matplotlib=True)
+    
+    
+    flat_shaplist = [item for sublist in shap_values[1] for item in sublist]
+    
+    interpret = pd.DataFrame()
+    interpret["name"] = features
+    interpret["SHAP"] = flat_shaplist#print(flat_shaplist)
+    plt.show()
+    # Explaining the 4th instance
+
+    return(interpret, y_proba, y_pred)
+
+
+def mol2svg(mol):
+    d2d = rdMolDraw2D.MolDraw2DSVG(200, 100)
+    d2d.DrawMolecule(mol)
+    d2d.FinishDrawing()
+    return d2d.GetDrawingText()
+
+
+    
+
+def main():
+    
+    st.set_page_config(
+    page_title="DILI Predictor",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    )
+    
+    #st.image("Logo.png", width=100)
+    st.title("DILI PRedictor")
+    st.write(
+    """
+    [![Follow](https://img.shields.io/twitter/follow/srijitseal?style=social)](https://www.twitter.com/srijitseal)
+    """
+)
+    
+    desc=pd.read_csv("all_features_desc.csv", encoding='windows-1252')
+    
+    smile=st.text_input("Enter SMILES")
+    smiles = unquote(smile)
+    smiles_r = standardized_smiles(smiles)
+    test = {'smiles_r':  [smiles_r] }
+    test = pd.DataFrame(test)
+    
+    
+    #predict
+    y_pred = ''
+    y_proba = ''
+    
+    if st.button('Predict DILI'):
+        
+        test_mfp_Mordred = calc_all_fp_desc(test)
+        test_mfp_Mordred_liv = predict_liv_all(test_mfp_Mordred)
+        test_mfp_Mordred_liv_values = test_mfp_Mordred_liv.T.reset_index().rename(columns={"index":"name", 0: "value"})
+
+        interpret, y_proba, y_pred = predict_DILI(test_mfp_Mordred_liv)   
+        interpret = pd.merge(interpret, desc, right_on="name", left_on="name", how="outer")
+        interpret = pd.merge(interpret, test_mfp_Mordred_liv_values, right_on="name", left_on="name", how="inner") 
+
+        print(y_proba[0])
+        print(y_pred[0]) 
+        
+        top = interpret[interpret["SHAP"]>0].sort_values(by=["SHAP"], ascending=False).reset_index(drop=True)
+        top = top[:1]
+        print(top)
+        
+        bottom = interpret[interpret["SHAP"]<0].sort_values(by=["SHAP"], ascending=True).reset_index(drop=True)
+        bottom = bottom[:1]
+        print(bottom)
+        
+    st.success(pred)
+
+if __name__ == '__main__': 
+    main()   
+
