207fbd7c420e59f29a7842dc71a557d9f72ac9fd
srijitseal <58182863+srijitseal@users.noreply.github.com>
2023-08-04 19:49:38 +0100

    Update Documentation.py

--- pages/Documentation.py
+++ pages/Documentation.py
@@ -8,6 +8,19 @@ st.set_page_config(
     initial_sidebar_state="expanded"
     )
 
+st.markdown(
+"""
+    ### DILI Predictor  Technology Overview 
 
+    DILI Predictor is an open-source app framework built specifically for
+    predicting human drug induced liver injury.
+    
+    DILI Predictor employs eleven proxy-DILI labels from in vitro (e.g., mitochondrial toxicity, bile 
+    salt export pump inhibition) and in vivo (e.g., preclinical rat hepatotoxicity studies) datasets 
+    along with structural fingerprints and physicochemical parameters as features.
+    
+    All our data and models are publicly available at GitHub and further details are published at bioaRxiv.
+"""
+)
 
 
